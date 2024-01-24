620593d69dae6a034ac625e3a997714384c70801
S4NJ33V <sanjeevrathan4444@gmail.com>
2024-01-24 15:56:32 +0530

    rele2

--- san.py
+++ san.py
@@ -1,5 +1,5 @@
 import sys
 from streamlit.web import cli as stcli
 if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "main.py", "5000"]
+    sys.argv = ["streamlit", "run", "main.py"]
     sys.exit(stcli.main())
